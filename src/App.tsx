f07e0f16d9e9e56f5ea8809529fd0fadaaac12a3
quietdreamr <kidfabi.97@gmail.com>
2023-04-24 18:17:03 +0200

    Avoiding merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Carousel from "./components/Carousel"
+import Login from "./components/Login";
+import catalogue from "./components/Catalogue";
+
 
 function App() {
   return (
@@ -14,6 +17,8 @@ function App() {
         <Routes>
           <Route path='/' Component={Home}></Route>
           <Route path='/test' element={<Carousel endpoint={"api/products/category/red"}></Carousel>}>
+          <Route path='/login' Component={Login}></Route>
+        <Route path='/catalogue' Component={catalogue}></Route>
           </Route>
         </Routes>
         <Footer />
