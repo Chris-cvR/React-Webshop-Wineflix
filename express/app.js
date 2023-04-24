f8558e910ff8ed94a8f8ec72ea1fa49c07b14ea4
quietdreamr <kidfabi.97@gmail.com>
2023-04-24 15:48:57 +0200

    Forgot one redundant endpoint

--- express/app.js
+++ express/app.js
@@ -6,11 +6,9 @@ const port = process.env.PORT || 8888;
 app.use(express.static('public'))
 
 const productsroute=require('./routes/api/products.js')
-const productroute=require('./routes/product.js')
 const cartroute=require('./routes/api/cart.js')
 
 app.use("/",productsroute)
-app.use("/",productroute)
 app.use("/",cartroute)
 
 app.listen(port, () => {
