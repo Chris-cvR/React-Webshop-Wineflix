286ec19cdfbe42585b35cfbd5324aa7ed2e6fc0b
quietdreamr <kidfabi.97@gmail.com>
2023-04-24 15:46:31 +0200

    Removed redundant content

--- express/app.js
+++ express/app.js
@@ -5,16 +5,10 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 
-const homeroute=require("./routes/home.js")
-const loginroute=require("./routes/login.js")
-const catalogueroute=require("./routes/catalogue.js")
 const productsroute=require('./routes/api/products.js')
 const productroute=require('./routes/product.js')
 const cartroute=require('./routes/api/cart.js')
 
-app.use("/",homeroute)
-app.use("/",loginroute)
-app.use("/",catalogueroute)
 app.use("/",productsroute)
 app.use("/",productroute)
 app.use("/",cartroute)
